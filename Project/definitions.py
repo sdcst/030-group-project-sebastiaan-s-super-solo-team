d3ad81f20b737a1322f2f108bfc1be1e38cc7619
Sebastiaan <bas.terkeurs@gmail.com>
2024-05-14 21:27:58 -0700

    done :+1:
    finished first six functions of calculator

--- Project/definitions.py
+++ Project/definitions.py
@@ -4,7 +4,7 @@ import math
 
 os.system('cls')
 def title():
-    
+    print("                     Sebastiaan's")
     print(" ,-----.      ,--.           ,--.        ,--.                  ")
     print("'  .--./,--,--|  |,---,--.,--|  |,--,--,-'  '-.,---.,--.--.         ") 
     print("|  |   ' ,-.  |  | .--|  ||  |  ' ,-.  '-.  .-| .-. |  .--'    ")
@@ -12,49 +12,39 @@ def title():
     print("`-----'`--`--`--'`---'`----'`--'`--`--' `--'  `---'`--'       ")
     print("===========================================================")
     time.sleep(1)
-    print("type '1' to")
+    print(" '1' to find volume of cube")
     time.sleep(1)
-    print("type '1' to")
+    print(" '2' to find volue of cone")
     time.sleep(1)
-    print("type '1' to")
+    print(" '3' to find volume of sphere")
     time.sleep(1)
-    print("type '1' to")
+    print(" '4' to find surface area of cube")
     time.sleep(1)
-    print("type '1' to")
+    print(" '5' to find surface area of cone")
     time.sleep(1)
-    print("type '1' to")
-    time.sleep(1)
-    print("type '1' to")
+    print(" '6' to find surface area of sphere")
     time.sleep(1)
    
-
-
-
 def volCube(num):
-    os.system('cls')
-    num = pow(num,3)
-    print(num)
-    input()
+    print("volume of cube = ",pow(num,3))
+    input("press 'enter' to return")
 
-def volCone():
-    x = input("\n\n    enter a number ")
-    x = x**3
-    print(x)
-    input()
-def volSphere(x):
-    x = input("\n\n     ")
+def volCone(radius, height):
+    print("volume of cone = ", 3.14*radius**2*(height / 3))
+    input("press 'enter' to return")
 
+def volSphere(radius):
+    print("volume of sphere = ", (4/3*3.14*radius**3))
+    input("press 'enter' to return")
 
-def saCube(x):
-    x = input("\n\n     ")
+def saCube(side):
+    print("surace area of cube = ", (side**2)*6)
+    input("press 'enter' to return")
 
-
-def saCone(x):
-    x = input("\n\n     ")
-
-
-def saCube(x):
-    x = input("\n\n     ")
-
-x = float(input())
-volCube(x)
+def saCone(radius, height):
+    print("surface area of cone = ", 3.14*radius*(radius+(height*2+radius*2)**0.5))
+    input("press 'enter' to return")
+  
+def saSphere(radius):
+    print("surface area of sphere = ", 4*3.14*radius**2)
+    input("press 'enter' to return")
--- Project/run.py
+++ Project/run.py
@@ -1,25 +1,31 @@
 import time, os, random, math, definitions
-os.system('cls')
-definitions.title()
-choice = input()
+z=0
+while z !=1:
+    os.system('cls')
+    definitions.title()
 
-if choice == 1:
-    x = float(input("input a side-length: "))
-    definitions.volCube(x)
-if choice == 2:
-    x = float(input("input a radius: "))
-    y = float(input("input a height: "))
-    definitions.volCone(x, y)
-if choice == 3:
-    x = float(input("input a radius: "))
-    definitions.volSphere(x)
-if choice == 4:
-    x = float(input("input a side length: "))
-    definitions.saCube(x)
-if choice == 5:
-    x = float(input("input a radius: "))
-    y = float(input("input a height: "))
-    definitions.saCone(x, y)
-if choice == 6:
-    x = float(input("input a radius: "))
-    definitions.saSphere(x)
+    choice = int(input("enter an option: "))
+    while choice == 0 or choice > 6 or choice < 0:
+        print("\n\n\n           invalid choice...")
+        time.sleep(3)
+        choice = int(input("enter an option: "))
+    if choice == 1:
+        x = float(input("input a side-length: "))
+        definitions.volCube(x)
+    if choice == 2:
+        x = float(input("input a radius: "))
+        y = float(input("input a height: "))
+        definitions.volCone(x, y)
+    if choice == 3:
+        x = float(input("input a radius: "))
+        definitions.volSphere(x)
+    if choice == 4:
+        x = float(input("input a side length: "))
+        definitions.saCube(x)
+    if choice == 5:
+        x = float(input("input a radius: "))
+        y = float(input("input a height: "))
+        definitions.saCone(x, y)
+    if choice == 6:
+        x = float(input("input a radius: "))
+        definitions.saSphere(x)
