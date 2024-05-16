91936b7096f1b40742497f9c797c271cf2ce5742
Sebastiaan <bas.terkeurs@gmail.com>
2024-05-16 12:34:45 -0700

    Final edition :smile:
    finally squarshed "string" bug.

--- run.py
+++ run.py
@@ -1,36 +1,37 @@
 import time, os, random, math
 import definitions
+##perma-loop
 z=0
 while z !=1:
     os.system('cls')
     definitions.title()
-
-    choice = int(input("enter an option: "))
-    while choice == 0 or choice > 6 or choice < 0:
+    try:
+        choice = int(input("enter an option: "))
+        while choice == 0 or choice > 6 or choice < 0:
+            print("\n\n\n           invalid choice...")
+            time.sleep(3)
+            choice = int(input("enter an option: "))
+        if choice == 1:
+            x = float(input("input a side-length: "))
+            definitions.volCube(x)
+        if choice == 2:
+            x = float(input("input a radius: "))
+            y = float(input("input a height: "))
+            definitions.volCone(x, y)
+        if choice == 3:
+            x = float(input("input a radius: "))
+            definitions.volSphere(x)
+        if choice == 4:
+            x = float(input("input a side length: "))
+            definitions.saCube(x)
+        if choice == 5:
+            x = float(input("input a radius: "))
+            y = float(input("input a height: "))
+            definitions.saCone(x, y)
+        if choice == 6:
+            x = float(input("input a radius: "))
+            definitions.saSphere(x)
+    except:
         print("\n\n\n           invalid choice...")
         time.sleep(3)
-        choice = int(input("enter an option: "))
-    if choice == 1:
-        x = float(input("input a side-length: "))
-        definitions.volCube(x)
-    if choice == 2:
-        x = float(input("input a radius: "))
-        y = float(input("input a height: "))
-        definitions.volCone(x, y)
-    if choice == 3:
-        x = float(input("input a radius: "))
-        definitions.volSphere(x)
-    if choice == 4:
-        x = float(input("input a side length: "))
-        definitions.saCube(x)
-    if choice == 5:
-        x = float(input("input a radius: "))
-        y = float(input("input a height: "))
-        definitions.saCone(x, y)
-    if choice == 6:
-        x = float(input("input a radius: "))
-        definitions.saSphere(x)
-    else:
-        print("\n\n\n           invalid choice...")
-        time.sleep(3)
-        choice = int(input("enter an option: "))
+        exit
